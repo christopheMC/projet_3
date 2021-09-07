8487744ef718ddb580f3b57619ad50444576100f
christophe cherif <krisfromtama@gmail.com>
2021-09-07 12:38:21 +0200

    Ajout docker-compose

---
+++ docker_fill/insert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+i=1
+sp="/-\|"
+echo -n ' '
+
+while read f1
+do
+   printf "\b${sp:i++%${#sp}:1}"
+   f1=$( echo "$f1" | tr -d "\r")
+   curl -s -X POST "elasticsearch:9200/projet_3/_doc" -H "Content-Type: application/json" -d "{ \"csv_line\": \"$f1\" }" > /tmp/output.html
+done < top250-00-19_modified.csv
